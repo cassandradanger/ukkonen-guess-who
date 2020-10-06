b05b39950466a54b6fdbe3fcbf61615d234be65b
Casie Siekman <casie.siekman@gmail.com>
2020-10-06 11:40:05 -0500

    add comparison

--- client.js
+++ client.js
@@ -11,6 +11,8 @@ console.log('Here are all the available people:', people);
 // <img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris">
 // ```
 
+let randomPerson = '';
+
 $(document).ready(onReady);
 
 function onReady(){
@@ -18,14 +20,33 @@ function onReady(){
     //append divs for all in people
     appendPeople();
     pickRandomPerson();
+    $('#githubPeople').on('click', '.clickPic', clickedPerson)
+}
+
+function clickedPerson(){
+    let clickedPerson = $(this).data('name');
+    console.log('clicked person and random', clickedPerson, randomPerson);
+
+    // compare the two names
+    // compare clickedPerson to the randomPerson
+
+    if( clickedPerson == randomPerson){
+        alert (`yes!! this is ${randomPerson}`);
+        pickRandomPerson();
+    } else {
+        alert('nope, try again');
+    }
+
 }
 
 
 function appendPeople(){
     for ( let i = 0; i< people.length; i++){
         $('#githubPeople').append(`
-            <li>
-                <img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}">
+            <li data-name=${people[i].name} class="clickPic">
+                <img src="https://github.com/${people[i].githubUsername}.png?size=250" 
+                alt="Profile image of ${people[i].name}" 
+                />
             </li>
         `);
     }
@@ -35,10 +56,13 @@ function pickRandomPerson(){
     let randomNum = randomNumber(0, people.length - 1);
     console.log('random number', randomNum);
 
-    $('#person').append(people[randomNum].name);
+    randomPerson = people[randomNum].name;
+    // $('#person').empty();
+    $('#person').text(randomPerson);
 }
 
 
 function randomNumber(min, max){
+   console.log( (1 + max - min) + min);
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
